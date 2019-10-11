02bf24d450c779ae4e52541aed0c004179a5ea7a
mtzulu27 <zumangi2007@gmail.com>
2019-10-11 08:31:02 -0500

    Update todolist.js

--- todolist.js
+++ todolist.js
@@ -10,3 +10,31 @@ app.listen(3000, function () {
 });
 
 
+app.post('/origamifire/:projectName/DELETE/:index', function(req, res){
+  for (var dataObject in req.body) {
+    obj = JSON.parse(dataObject);
+    var itemToUpdate = Object.keys(obj)[req.params.index];
+    deleteItemFromFirebase(req.params.projectName, itemToUpdate)
+  }
+  res.sendStatus(200)
+  var error = function (err, response) {
+    res.send(err)
+  };
+})
+
+function deleteItemFromFirebase(projectName, id){
+  var options = {
+    headers: {
+      'X-HTTP-Method-Override': 'DELETE'
+    }
+  };
+  var url = 'https://'+ projectName + '.firebaseio.com/todos/'+ id +'.json'
+  request.get(url,options,function(err,res,body){
+    if(err){
+      console.log(err)
+    }
+    if(res.statusCode !== 200 ){
+      console.log('success')
+    }
+  });
+}
